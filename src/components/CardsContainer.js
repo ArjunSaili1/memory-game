727d786977a7de0c820c714e147974ab843aa1a7
Arjun Saili <arjun7.saili@gmail.com>
2022-03-18 22:09:58 -0400

    add card shuffle function

--- src/components/CardsContainer.js
+++ src/components/CardsContainer.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "./Card";
-import players from "../assets/index";
+import playersObj from "../assets/index";
 
 function CardsContainer(props){
+
+    const [players, setPlayers] = useState(playersObj)
+
+    const shufflePlayers = () => {
+        let playersCopy = players;
+        let current = playersCopy.length;
+        let randomIndex;
+        while(current !== 0){
+            randomIndex = Math.floor(Math.random()* current);
+            current--;
+            [playersCopy[current], playersCopy[randomIndex]] = [playersCopy[randomIndex], playersCopy[current]]
+        }
+        setPlayers([...playersCopy]);
+    }
 
     return(
         <div className="card-outer-container">
