9a098fc15a9767e0f3cb5089b9fb52920c8c608f
Arjun Saili <arjun7.saili@gmail.com>
2022-03-18 20:58:30 -0400

    Add Images, Create export obj

---
+++ src/assets/index.js
@@ -0,0 +1,14 @@
+export {default as lebronjames} from './lebronjames.jpg'
+export {default as michaeljordan} from './jordan.jpg'
+export {default as kobebryant} from './kobebryant.jpg'
+export {default as shaquilleoneal} from './shaq.jpg'
+export {default as wiltchamberlain} from './wilt.jpg'
+export {default as larrybird} from './bird.jpg'
+export {default as magicjohnson} from './magic.jpg'
+export {default as kareemjabbar} from './kareem.jpg'
+export {default as hakeemlajuwon} from './hakeem.jpg'
+export {default as timduncan} from './duncan.jpg'
+export {default as billrussell} from './bill.jpg'
+export {default as stephencurry} from './steph.jpg'
+
+
---
+++ src/assets/players.js
@@ -0,0 +1,29 @@
+import { lebronjames } from ".";
+import { michaeljordan } from ".";
+import { kobebryant } from ".";
+import { shaquilleoneal } from ".";
+import { wiltchamberlain } from ".";
+import { larrybird } from ".";
+import { magicjohnson } from ".";
+import { kareemjabbar } from ".";
+import { hakeemlajuwon } from ".";
+import { timduncan } from ".";
+import { billrussell } from ".";
+import { stephencurry } from ".";
+
+const players = [
+    {name: "LeBron James", image: lebronjames},
+    {name: "Michael Jordan", image: michaeljordan},
+    {name: "Kobe Bryant", image: kobebryant},
+    {name: "Shaquille O'neal", image: shaquilleoneal},
+    {name: "Wilt Chamberlain", image: wiltchamberlain},
+    {name: "Larry Bird", image: larrybird},
+    {name: "Magic Johnson", image: magicjohnson},
+    {name: "Kareem Abdul Jabbar", image: kareemjabbar},
+    {name: "Hakeem Olajuwon", image: hakeemlajuwon},
+    {name: "Tim Duncan", image: timduncan},
+    {name: "Bill Russell", image: billrussell},
+    {name: "Stephen Curry", image: stephencurry}
+]
+
+export default players;
